5ca5522e62a8bd9e4bf63097fcb1e4b1401f8ff4
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-06-10 13:09:38 +0100

    Create associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
-    belongs_to :user
+  belongs_to :user
+  has_many :attended_events
+  has_many :users, through: :attended_events
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :events
+  has_many :attended_events
+  has_many :events, through: :attended_events
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
