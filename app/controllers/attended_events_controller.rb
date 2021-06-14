00b5b74c59b35f9bc9e36f512bccfa6550f0cc7d
Igor.Koloski <koloski_igor@hotmail.com>
2021-06-14 11:13:55 +0200

    Fix linter errors

--- app/controllers/attended_events_controller.rb
+++ app/controllers/attended_events_controller.rb
@@ -11,14 +11,14 @@ class AttendedEventsController < ApplicationController
       return
     end
     @attended_event = AttendedEvent.new(event_id: event.id, user_id: current_user.id)
-    if AttendedEvent.where(user_id: current_user.id, event_id: params[:event_id]).exists? 
-      redirect_to event_path(event) 
-      return 
+    if AttendedEvent.where(user_id: current_user.id, event_id: params[:event_id]).exists?
+      redirect_to event_path(event)
+      return
     end
     if event.user_id == current_user.id
-      redirect_to event_path(event) 
-      return 
-    end 
+      redirect_to event_path(event)
+      return
+    end
     redirect_back(fallback_location: root_path) if @attended_event.save
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates_uniqueness_of :email, presence: true
-         validates :password, presence: true
+  validates_uniqueness_of :email, presence: true
+  validates :password, presence: true
 end
