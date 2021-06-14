6086ecaf1526bd27499559618e046760eaa1e2e5
Igor.Koloski <koloski_igor@hotmail.com>
2021-06-14 11:02:57 +0200

    Add README.MD file

--- app/controllers/attended_events_controller.rb
+++ app/controllers/attended_events_controller.rb
@@ -6,6 +6,10 @@ class AttendedEventsController < ApplicationController
 
   def create
     event = Event.find(params[:event_id])
+    unless user_signed_in?
+      redirect_to new_user_session_path
+      return
+    end
     @attended_event = AttendedEvent.new(event_id: event.id, user_id: current_user.id)
     if AttendedEvent.where(user_id: current_user.id, event_id: params[:event_id]).exists? 
       redirect_to event_path(event) 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!, :except => [:sign_in]
+
     def show
-        @event = Event.where('user_id = ?', current_user.id)
+        @event = Event.where('user_id = ?', current_user)
         # @attended = AttendedEvent.where('user_id = ?', current_user.id)
     end
 end
